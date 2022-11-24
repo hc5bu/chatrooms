63e263261852f2192080f64b253c3afa148a301d
hc5bu <hc5bu@virginia.edu>
2022-11-24 13:09:09 -0500

    Added functions

---
+++ netlify/functions/cleanup.ts
@@ -0,0 +1,64 @@
+const { schedule } = require('@netlify/functions');
+
+const handler = async function (event, context) {
+    console.log("Received event:", event);
+    const resp = await fetch("https://chatroom-2bf9b-default-rtdb.firebaseio.com/.json", {
+        method: "GET",
+    });
+    if (!resp.ok) {
+        console.error("Error:", resp.json());
+        return {
+            statusCode: resp.status,
+        }
+    }
+    const data = resp.json();
+    const deleteList = [];
+    for (const id in data) {
+        const numUsers = data[id]['users'] !== undefined ? Object.keys(data[id]['users']).length : 0;
+        const lastUpdated = data[id]['lastUpdated'];
+        const now = Date.now()
+        if (lastUpdated !== undefined && now - lastUpdated > 1200000 && numUsers === 0)
+            deleteList.push(id);
+    }
+
+    if (deleteList.length === 0) {
+        console.log("Nothing to delete.");
+        return {
+            statusCode: 200,
+        };
+    } else {
+        console.log("To delete: ", deleteList);
+        const update = {};
+        for (const id of deleteList) {
+            update[id] = {};
+            update[id]['lastUpdated'] = null;
+            update[id]['messages'] = null;
+        }
+        let response: Response;
+        try {
+            response = await fetch('https://chatroom-2bf9b-default-rtdb.firebaseio.com/.json', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(update),
+            });
+        } catch (error) {
+            console.error('Error: ', error);
+            return {
+                statusCode: 400,
+            };
+        }
+        const rj = await response.json();
+        if (!response.ok)
+            console.error("Error:", rj);
+        else
+            console.log('Successfully updated:', rj);
+        return {
+            statusCode: response.status,
+        };
+    }
+
+};
+
+exports.handler = schedule("@hourly", handler);
