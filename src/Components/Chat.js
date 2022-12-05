04752273362f0e103fba0eac1b20b40f37fe4624
hc5bu <hc5bu@virginia.edu>
2022-12-05 12:44:09 -0500

    tried resizing chat based on window size

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "../Styles/Chat.css";
 
 function Chat(props) {
@@ -6,6 +6,7 @@ function Chat(props) {
     //It simply renders the chat based on the data it's provided
 
     const bottomRef = useRef(null);
+    const { width } = useWindowSize();
 
     useEffect(() => {
         //scroll to bottom at beginning. Does this before images are all loaded
@@ -24,12 +25,12 @@ function Chat(props) {
     if (props.messages.length === 0) {
         return (
             <div id="nothing">
-                <div>
+                <div style={{ fontSize: (width >= 1200 ? "1.5em" : "1em") }}>
                     There haven't been any messages sent here yet.<br />
                     Be the first to send a message!
                 </div>
                 {props.error !== undefined &&
-                    <div className="chatError">
+                    <div className="chatError" style={{ fontSize: (width >= 1200 ? "1.5em" : "1em") }}>
                         Something went wrong. Please refresh the page. <br />
                         Reason: {props.error}
                     </div>}
@@ -49,24 +50,24 @@ function Chat(props) {
         else
             return t.toLocaleDateString("en-US");
     })
-    
+
     // props.messages should be passed in as a list, has username, text, timestamp field for each
     // unless it's a file message, which involves more work
     const parseContent = (message) => {
         if (message.text !== undefined) {
             return (
-                <div className="messageText messageMiddle">
+                <div className="messageText messageMiddle" style={{ fontSize: (width >= 1200 ? "1.5em" : "1em") }}>
                     {message.text}
                 </div>
             )
         } else {
             // it's a file
-            if(message.filetype.startsWith("image"))
-                return(<img className="messageImage messageMiddle" src={message.fileUrl} alt={message.filename}/>);
-            else if(message.filetype.startsWith("audio"))
-                return(
+            if (message.filetype.startsWith("image"))
+                return (<img className="messageImage messageMiddle" src={message.fileUrl} alt={message.filename} />);
+            else if (message.filetype.startsWith("audio"))
+                return (
                     <audio className="messageAudio messageMiddle" controls>
-                        <source src={message.fileUrl}/>
+                        <source src={message.fileUrl} />
                     </audio>
                 )
             else
@@ -82,11 +83,11 @@ function Chat(props) {
                     return (
                         <div className={message.username === props.username ? "myMessage" : "notMyMessage"}
                             ref={i === props.messages.length - 1 ? bottomRef : undefined} key={i} id={`message-${i}`}>
-                            <div className="username">
+                            <div className="username" style={{ fontSize: (width >= 1200 ? "21px" : "14px") }}>
                                 {message.username}
                             </div>
                             {parseContent(message)}
-                            <div className="timestamp">
+                            <div className="timestamp" style={{ fontSize: (width >= 1200 ? "15px" : "10px") }}>
                                 {dates[i]}&nbsp;{times[i]}
                             </div>
                         </div>
@@ -103,6 +104,32 @@ function Chat(props) {
             <div id="spacer"></div>
         </div>
     );
+}
+
+// Hook
+function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        // Handler to call on window resize
+        function handleResize() {
+            // Set window width/height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
 }
 
 export default Chat;
